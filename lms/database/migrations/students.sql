7df0f53fe506c19eea3fbd220c59f247442e82a9
ankit gupta <meankitgupta.gupta@gmail.com>
2019-12-25 13:09:18 +0530

    update student migration

--- lms/database/migrations/students.sql
+++ lms/database/migrations/students.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS `students` (
     `academy_address` VARCHAR(191) DEFAULT NULL,
     `academy_city` VARCHAR(50) DEFAULT NULL,
     `academy_pincode` VARCHAR(8) DEFAULT NULL,
-    `academy_type` VARCHAR(10) DEFAULT NULL,
+    `academy_type` VARCHAR(50) DEFAULT NULL,
     `academy_standard` SMALLINT(5) DEFAULT NULL,
     `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at` TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
