37da7fb166d83709eee0cb6c8ce79859e391663f
ankit gupta <meankitgupta.gupta@gmail.com>
2020-01-24 01:43:12 +0530

    display form errors

--- front/angular/src/app/components/loader/loader.component.ts
+++ front/angular/src/app/components/loader/loader.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoaderService } from 'src/app/services/loader/loader.service';
 
 @Component({
   selector: 'app-loader',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoaderComponent implements OnInit {
 
-  constructor() { }
+  loading:boolean;
+
+  constructor(private _loader:LoaderService) { }
 
   ngOnInit() {
+    this._loader.status.subscribe((status:boolean) => {
+      this.loading = status;
+    })
   }
 
 }
--- front/angular/src/app/modules/shared/components/show-errors/show-errors.component.ts
+++ front/angular/src/app/modules/shared/components/show-errors/show-errors.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { AbstractControlDirective, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-show-errors',
@@ -9,7 +10,42 @@ export class ShowErrorsComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  @Input() private control:AbstractControlDirective | AbstractControl;
+
+  ngOnInit() { }
+
+  private static readonly errorMessages = {
+    'required': (params) => '##FIELD## can\'t be blank',
+    'minlength': (params) => '##FIELD## should be minimum '+params.requiredLength+' characters',
+    'maxlength': (params) => '##FIELD## should not be greater then '+params.requiredLength+' characters',
+    'pattern': (params) => '##FIELD## should be a valid',
+    'email': (params) => "##FIELD## should be vaild email."
+  };
+
+  display():boolean {
+    return this.control && this.control.errors && (this.control.dirty || this.control.touched);
   }
 
+  getError(): string {
+    //console.log("show",this.control.errors);
+    let errors = Object.keys(this.control.errors)
+        .map(field => this.getMessage(field, this.control.errors[field],this.control)
+      );
+
+    return errors[0];
+  }
+
+  private getMessage(type: string, params: any,control:any) {
+    let fname = this.getControlName(control);
+    fname = fname.replace("_"," ").replace(" id","").toLowerCase();
+    fname = fname.replace(/\b\w/g, l => l.toUpperCase());
+    let msg = ShowErrorsComponent.errorMessages[type](params);
+    return msg.replace("##FIELD##",fname);
+  }
+
+
+  getControlName(c: AbstractControl): string | null {
+    const formGroup = c.parent.controls;
+    return Object.keys(formGroup).find(name => c === formGroup[name]) || null;
+  }
 }
