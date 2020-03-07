6e6335bf4867598b18c16941c73622bea1fedf18
ankit gupta <meankitgupta.gupta@gmail.com>
2020-03-07 14:38:08 +0530

    delete code

--- front/angular/src/app/helpers/remove-null-values.ts
+++
@@ -1,7 +0,0 @@
-export function RemoveNullObject (values:object):object {
-    Object.keys(values).forEach(key => {
-      if (values[key] && typeof values[key] === 'object') RemoveNullObject (values[key]);
-      else if (values[key] == null || !(String (values[key]).trim()).length) delete values[key];
-    });
-  return values;
-}
