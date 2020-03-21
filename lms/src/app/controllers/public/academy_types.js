79e076647dd9fc37af0994131ca5c4a6740d3780
ankit gupta <meankitgupta.gupta@gmail.com>
2020-03-21 21:31:47 +0530

    update acadmic types message

--- lms/src/app/controllers/public/academy_types.js
+++ lms/src/app/controllers/public/academy_types.js
@@ -1,5 +1,5 @@
 const academic_types = require('../../enum/academic_type');
 
 module.exports = (req, res) => {
-    return res.send (200, {status: true, message: 'Genres', data: {...academic_types}}) ;
+    return res.send (200, {status: true, message: 'Academic Types', data: {...academic_types}}) ;
 }
